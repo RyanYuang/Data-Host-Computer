46c5ee4b5ef6b7150ca79c9394cda035b314ac03
yuangryan <RyanYuang@outlook.com>
2026-01-10 23:27:21 +0800

    完成HeadView的UI开发

--- Src/UI/MainPage/Head/HeadView.py
+++ Src/UI/MainPage/Head/HeadView.py
@@ -1,45 +1,94 @@
 import PyQt6
-from PyQt6.QtCore import QSize
-from PyQt6.QtGui import QFont
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton
-
+from PIL.ImageQt import QPixmap
+from PyQt6.QtCore import QSize 
+from PyQt6.QtGui import QFont,QIcon
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton, QSpacerItem, QSizePolicy ,QHBoxLayout
 
 class HeadView(QWidget):
     def __init__(self, parent=None):
         super(QWidget, self).__init__(parent)
         # 定义组件
-        layout = None
-        TitleLable = None
-        ConnectButton = None
-        AlarmButton = None
-        SettingButton = None
-
+        self.layout = None
+        self.TitleLabel = None
+        self.ConnectButton = None
+        self.AlarmButton = None
+        self.SettingButton = None
 
         self.InitUI()
 
-
     def InitUI(self):
         """
-        @berif 初始化UI
+        @brief 初始化UI
         :return:
         """
-        TitleLable = QLabel(self)
-        TitleLable.setText("环境监测控制系统")
+        # 创建标签
+        self.TitleLabel = QLabel(self)
+        self.TitleLabel.setText("环境监测控制系统")
         Font = QFont()
         Font.setBold(True)
         Font.setPointSize(30)
-        TitleLable.setFont(Font)
-        TitleLable.setStyleSheet("""
-            Color:rgb(0, 0, 0);
-             """)
-        # spacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-        ConnectButton = QPushButton(self)
-        ConnectButton.resize(125,40)
+        self.TitleLabel.setFont(Font)
+        self.TitleLabel.setStyleSheet("""
+            color: rgb(0, 0, 0);
+            """)
 
-        AlarmButton = QPushButton(self)
+        # 创建按钮
+        self.ConnectButton = QPushButton("连接", self)
+        self.ConnectButton.setStyleSheet("""
+            Color:rgb(193,0,7);
+            background-color: rgb(255,226,226);
+            border-radius: 10px;
+            width: 125px;
+            height: 40px;
+        """)
+        self.AlarmButton = QPushButton("正常", self)
+        AlarmIcon = QIcon()
+        AlarmIcon.addFile("Resource/AlarmNormal.png",state=QIcon.State.Off)
+        AlarmIcon.addFile("Resource/AlarmTrigger.png",state=QIcon.State.On)
+        self.AlarmButton.setIcon(AlarmIcon)
+        self.AlarmButton.setStyleSheet("""
+            QPushButton {
+                border-radius: 10px;
+                height: 40px;
+                width: 92px;
+            }
+            QPushButton:checked {
+                color: rgb(193,0,7);
+                background-color: rgb(255, 226, 226); /* 按下时显示红色背景 */
+            }
+            QPushButton:!checked 
+            {
+                color: rgb(74,85,101);
+                background-color: rgb(243,244,246);
+            }
+        """)
+        self.AlarmButton.setCheckable(True)
 
+        SettingIcon = QIcon("Resource/SettingIcon.png")
+        self.SettingButton = QPushButton(self)
+        self.SettingButton.setIcon(SettingIcon)
+        self.SettingButton.setStyleSheet("""
+        QPushButton {
+            width: 44px;
+            height: 44px;
+            border-radius: 10px;
+            background-color: rgb(255,255,255);
+        }
+        """)
 
-        layout = QVBoxLayout()
-        layout.addWidget(TitleLable)
-        # layout.addItem(spacer)
-        layout.addWidget(ConnectButton)
+        # 创建布局
+        self.layout = QHBoxLayout()
+        
+        # 添加标题
+        self.layout.addWidget(self.TitleLabel)
+        
+        # 添加弹性空间，使按钮靠底部
+        self.layout.addStretch()  # 或者使用这个简单的伸缩器
+        
+        # 添加按钮
+        self.layout.addWidget(self.ConnectButton)
+        self.layout.addWidget(self.AlarmButton)
+        self.layout.addWidget(self.SettingButton)
+        
+        # 设置布局
+        self.setLayout(self.layout)
--- Src/UI/MainPage/MainPage.py
+++ Src/UI/MainPage/MainPage.py
@@ -11,14 +11,16 @@ class MainPage(QWidget):
     def initUI(self):
         # 设置背景颜色
         palette = self.palette()
-        palette.setColor(QPalette.ColorRole.Window, QColor(235, 240, 253))  # 设置浅灰色背景
+        palette.setColor(QPalette.ColorRole.Window, QColor(190, 219, 255))  # 设置浅灰色背景
         self.setPalette(palette)
         self.setAutoFillBackground(True)  # 启用自动填充背景
 
         # 初始化组件
         head_view = HeadView()  # 创建 HeadView 的实例
+        head_view.resize(self.width(), 68)
 
         # 初始化layout
         layout = QVBoxLayout()
+        layout.setAlignment(Qt.AlignmentFlag.AlignTop)
         layout.addWidget(head_view)  # 将 HeadView 实例添加到布局中
         self.setLayout(layout)
