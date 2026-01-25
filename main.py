12b722e47cbb810b0a8241a06b5e6f2b0d8514bb
yuangryan <RyanYuang@outlook.com>
2026-01-25 18:30:34 +0800

    补充上一个提交

--- main.py
+++ main.py
@@ -3,14 +3,27 @@ from PyQt6.QtWidgets import QApplication, QMainWindow
 from Src.Startup.StartupManager import run_startup
 from Src.UI.MainPage.MainPage import MainPage  # 修改导入方式
 from Src.UI.MainPage.MainPagePresenter import MainPagePresenter
-
+from Src.Manager.ThreadManager import ThreadManager
+from Src.Serial.SerialThread import SerialThread
+from Src.Serial.SerialManger import SerialManger
+from Src.DataEngine.CarDataEngine import CarDataEngine
 
 if __name__ == "__main__":
     exit_code = run_startup(sys.argv)
     if exit_code is not None:
         sys.exit(exit_code)
-
     app = QApplication(sys.argv)
+    # 创建数据引擎
+    data_engine = CarDataEngine()
+
+    # 创建线程管理器
+    thread_manager = ThreadManager()
+    # 创建串口线程
+    serial_thread = SerialThread(name="SerialThread-1")
+    thread_manager.start_worker("serial_thread", serial_thread, serial_thread.start)
+    serial_thread.BindDataEngine(data_engine)
+
+
     # QMainWindow是PyQt6中主窗口类
     main_window = QMainWindow()
     main_page = MainPage()  # 创建MainPage实例
